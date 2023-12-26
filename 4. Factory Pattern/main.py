629ddace2e0048fc8229a33f0e153b5c3fc312e2
Dilik <1989deny@gmail.com>
2023-12-26 11:52:35 -0500

    modifying the code

--- 4. Factory Pattern/main.py
+++ 4. Factory Pattern/main.py
@@ -1,6 +1,113 @@
 from abc import ABC, abstractmethod
 
 
+# Pizza ingredient factory interface
+class PizzaIngredientFactory(ABC):
+    @abstractmethod
+    def create_dough(self):
+        pass
+
+    @abstractmethod
+    def create_sauce(self):
+        pass
+
+    @abstractmethod
+    def create_cheese(self):
+        pass
+
+
+class Dough(ABC):
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class Sauce(ABC):
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class Cheese(ABC):
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class Clam(ABC):
+    @abstractmethod
+    def __str__(self):
+        pass
+
+
+class ThinCrustDough(Dough):
+    def __str__(self):
+        return "Thin Crust Dough"
+
+
+class ThickCrustDough(Dough):
+    def __str__(self):
+        return "Thick Crust Dough"
+
+
+class PlumTomatoSauce(Sauce):
+    def __str__(self):
+        return "Plum Tomato Sauce"
+
+
+class MarinaraSauce(Sauce):
+    def __str__(self):
+        return "Marinara Sauce"
+
+
+class MozzarellaCheese(Cheese):
+    def __str__(self):
+        return "Mozzarella Cheese"
+
+
+class ReggianoCheese(Cheese):
+    def __str__(self):
+        return "Reggiano Cheese"
+
+
+class FrozenClam(Clam):
+    def __str__(self):
+        return "Frozen Clam"
+
+
+class FreshClam(Clam):
+    def __str__(self):
+        return "Fresh Clam"
+
+
+class NYPizzaIngredientFactory(PizzaIngredientFactory):
+    def create_dough(self):
+        return ThinCrustDough()
+
+    def create_sauce(self):
+        return MarinaraSauce()
+
+    def create_cheese(self):
+        return ReggianoCheese()
+
+    def create_clam(self):
+        return FreshClam()
+
+
+class ChicagoPizzaIngredientFactory(PizzaIngredientFactory):
+    def create_dough(self):
+        return ThickCrustDough()
+
+    def create_sauce(self):
+        return PlumTomatoSauce()
+
+    def create_cheese(self):
+        return MozzarellaCheese()
+
+    def create_clam(self):
+        return FrozenClam()
+
+
 # Pizza Interfaces
 class Pizza(ABC):
     @abstractmethod
@@ -22,22 +129,27 @@ class Pizza(ABC):
 
 # Concrete Pizza Products
 class NYStyleCheesePizza(Pizza):
+    def __init__(self, ingredient_factory):
+        self.ingredient_factory = ingredient_factory
+        self.dough = None
+        self.sauce = None
+        self.cheese = None
+
     def prepare(self):
         print("Preparing NY Style Cheese Pizza")
+        self.dough = self.ingredient_factory.create_dough()
+        self.sauce = self.ingredient_factory.create_sauce()
+        self.cheese = self.ingredient_factory.create_cheese()
 
-    def bake(self):
-        print("Bake for 25 minutes at 350")
-
-    def cut(self):
-        print("Cutting the pizza into diagonal slices")
-
-    def box(self):
-        print("Place pizza in official PizzaStore box")
-
-
-class NYStylePepperoniPizza(Pizza):
-    def prepare(self):
-        print("Preparing NY Style Pepperoni Pizza")
+        print(
+            "Ingredients"
+            + "\n"
+            + str(self.dough)
+            + "\n"
+            + str(self.sauce)
+            + "\n"
+            + str(self.cheese)
+        )
 
     def bake(self):
         print("Bake for 25 minutes at 350")
@@ -50,22 +162,27 @@ class NYStylePepperoniPizza(Pizza):
 
 
 class ChicagoStyleCheesePizza(Pizza):
+    def __init__(self, ingredient_factory):
+        self.ingredient_factory = ingredient_factory
+        self.dough = None
+        self.sauce = None
+        self.cheese = None
+
     def prepare(self):
         print("Preparing Chicago Style Cheese Pizza")
+        self.dough = self.ingredient_factory.create_dough()
+        self.sauce = self.ingredient_factory.create_sauce()
+        self.cheese = self.ingredient_factory.create_cheese()
 
-    def bake(self):
-        print("Bake for 40 minutes at 350")
-
-    def cut(self):
-        print("Cutting the pizza into square slices")
-
-    def box(self):
-        print("Place pizza in official PizzaStore box")
-
-
-class ChicagoStylePepperoniPizza(Pizza):
-    def prepare(self):
-        print("Preparing Chicago Style Pepperoni Pizza")
+        print(
+            "Ingredients"
+            + "\n"
+            + str(self.dough)
+            + "\n"
+            + str(self.sauce)
+            + "\n"
+            + str(self.cheese)
+        )
 
     def bake(self):
         print("Bake for 40 minutes at 350")
@@ -95,20 +212,20 @@ class PizzaStore(ABC):
 # Concrete PizzaStores
 class NYPizzaStore(PizzaStore):
     def create_pizza(self, pizza_type):
+        ingredient_factory = NYPizzaIngredientFactory()
+
         if pizza_type == "cheese":
-            return NYStyleCheesePizza()
-        elif pizza_type == "pepperoni":
-            return NYStylePepperoniPizza()
+            return NYStyleCheesePizza(ingredient_factory)
         else:
             raise ValueError("Error: Invalid type of pizza")
 
 
 class ChicagoPizzaStore(PizzaStore):
     def create_pizza(self, pizza_type):
+        ingredient_factory = ChicagoPizzaIngredientFactory()
+
         if pizza_type == "cheese":
-            return ChicagoStyleCheesePizza()
-        elif pizza_type == "pepperoni":
-            return ChicagoStylePepperoniPizza()
+            return ChicagoStyleCheesePizza(ingredient_factory)
         else:
             raise ValueError("Error: Invalid type of pizza")
 
@@ -121,5 +238,5 @@ if __name__ == "__main__":
     print("Ordering NY Style Cheese Pizza")
     ny_store.order_pizza("cheese")
 
-    print("Ordering Chicago Style Pepperoni Pizza")
-    chicago_store.order_pizza("pepperoni")
+    print("Ordering Chicago Style Cheese Pizza")
+    chicago_store.order_pizza("cheese")
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Strategy Pattern
 ----------------
 
+![Strategy Pattern](./Images/1_strategy_pattern.png)
+
 Strategy - defines a family of algorithms, encapsulates each one, and makes them interchangeable. Strategy lets the algorithm vary independently from clients that use it.
 
 1) Identify the aspects of your application that vary and separate them from what stays the same.
@@ -13,6 +15,8 @@ Strategy - defines a family of algorithms, encapsulates each one, and makes them
 Observer Pattern
 ----------------
 
+![Observer Pattern](./Images/2_observer_pattern.png)
+
 Observer - defines a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically
 
 1) Strive for loosely coupled designs between objects that interact.
@@ -20,6 +24,8 @@ Observer - defines a one-to-many dependency between objects so that when one obj
 
 Decorator Pattern
 -----------------
+
+![Decorator Pattern](./Images/3_decorator_pattern.png)
 
 Decorator - attaches additional responsibilities to an object dynamically. Decorators provide a flexible alternative to subclassing for extending functionality.
 
@@ -29,6 +35,8 @@ Decorator - attaches additional responsibilities to an object dynamically. Decor
 Factory Pattern
 -----------------
 
+![Factory Pattern](./Images/4_factory_pattern_a.png)
+![Factory Pattern](./Images/4_factory_pattern_b.png)
 Abstract Factory - Provides an interface for creating families of related or dependent objects without
 specifying their concrete classes.
 
